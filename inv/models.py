2bd1b768e047b1a3aa93a801cd9b0b8b2fdbc9f6
jyhmeepo <jyhqqqq@163.com>
2016-07-01 14:39:22 +0800

    none

--- inv/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Comp(models.Model):
-    name = models.CharField(max_length=255,default='',blank=True)
-    money = models.CharField(max_length=255,default='',blank=True)
-    def __str__(self):
-        return self.name
