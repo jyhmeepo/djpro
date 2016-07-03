03c5a2079cf1f6e1b73c531a4525f11d9623dd7f
jyhmeepo <jyhqqqq@163.com>
2016-07-03 11:19:43 +0800

    read1

--- read/views.py
+++ read/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import *
+from .models import *
 # Create your views here.
 
 def read(req):
     data = {}
 
-    return render(req,'')
-    return HttpResponse('read')
+    if req.method == "POST":
+        sname = req.POST['sname']
+        Series.objects.create(sname = sname)
+        return HttpResponseRedirect("/read/")
+    data['re'] = Series.objects.all()
+
+    return render(req,'read.html',data)
+    # return HttpResponse('read')
