491f0276e5da15db8a6a881664134a1d50c6a40a
jyhmeepo <jyhqqqq@163.com>
2016-07-04 17:39:07 +0800

    1

--- img/views.py
+++ img/views.py
@@ -4,17 +4,17 @@ from django.http import *
 
 
 
-# -----------------------------form-start--------------------------------------------
+# -----------------------------form-start--------------------------------------------s
 from django import forms
 
 class UpF(forms.Form):
-    iname = forms.CharField(max_length=255)
-    iurl = forms.FileField()
+    iname = forms.CharField(max_length=255,widget=forms.Textarea(attrs={'class': 'form-control'}))
+    file = forms.FileField(widget=forms.Textarea(attrs={'class': 'form-control'}))
 
 
-# -----------------------------from-end----------------------------------------------
+# -----------------------------from-end----------------------------------------------e
 
-# -----------------------------fucntion-start------------------------------------------
+# -----------------------------fucntion-start------------------------------------------s
 def handle_uploaded_file(f):
     """
     save upload file
@@ -32,9 +32,9 @@ def handle_uploaded_file(f):
             destination.write(chunk)
     return data
 
-# -----------------------------fucntion-end------------------------------------------
+# -----------------------------fucntion-end------------------------------------------e
 
-# -----------------------------view-start--------------------------------------------
+# -----------------------------view-start--------------------------------------------s
 def img(req):
     data={}
     data['url'] = image.objects.values().order_by("-id")
@@ -42,14 +42,16 @@ def img(req):
 
 def pub(req):
     data={}
+    # data['form'] = UpF()
     if req.method=="POST":
         if req.FILES:
             data['re'] = 123
             file = req.FILES['file']
             dd = handle_uploaded_file(file)
             data['url'] = dd['path']
-            data['form'] = UpF()
+
             image.objects.create(iname=req.POST['iname'],iurl=data['url'])
             # data['rr'] = file
+            return HttpResponseRedirect("/")
     return render(req,'pub.html',data)
-# -----------------------------view-end--------------------------------------------
+# -----------------------------view-end---------------------------------------------e
