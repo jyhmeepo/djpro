08cd6b4151b66bc37a3ad9e568738a3daa1e6405
jyhmeepo <jyhqqqq@163.com>
2016-07-19 14:10:31 +0800

    5

--- read/admin.py
+++ read/admin.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from read.models import *
 # Register your models here.
 
-admin.site.register(Series)
-class Perioda(admin.ModelAdmin):
-    list_display = ['pname','password']
-admin.site.register(Period,Perioda)
-admin.site.register(Image)
-admin.site.register(Pinfo)
-admin.site.register(Tags)
-admin.site.register(Video)
-admin.site.register(Wen)
+# admin.site.register(Series)
+# class Perioda(admin.ModelAdmin):
+#     list_display = ['pname','password']
+# admin.site.register(Period,Perioda)
+# admin.site.register(Image)
+# admin.site.register(Pinfo)
+# admin.site.register(Tags)
+# admin.site.register(Video)
+# admin.site.register(Wen)
